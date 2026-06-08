dea5522323f4d80a18684122321b103d05c09073
JhoanSLC <developergbp60@gmail.com>
2026-06-08 14:19:17 -0500

    feat(Products): :sparkles: Add product routes to index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from '../modules/auth/auth.routes';
 import reportRoutes from '../modules/reports/report.routes';
 import userRoutes from '../modules/users/user.routes';
 import categoryRoutes from '../modules/categories/category.routes';
+import productRoutes from '../modules/products/product.routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/auth', authRoutes);
 router.use('/reports', reportRoutes);
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
 
 export default router;
